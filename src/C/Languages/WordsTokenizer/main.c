be6bd6cc7c842599892eb01b31a4d2fd92071798
JoshuaLight <ashkariel.eter@gmail.com>
2019-10-18 19:07:35 +0300

    Cleaned up `main.c`.

--- src/C/Languages/WordsTokenizer/main.c
+++ src/C/Languages/WordsTokenizer/main.c
@@ -2,9 +2,7 @@
 #include "common/cursor.h"
 
 int main() {
-    StringCursor cursor = cursor_of("Hello");
-
-    printf("%c", current_of(cursor));
+    StringCursor cursor = cursor_of("Wello");
 
     return 0;
 }
